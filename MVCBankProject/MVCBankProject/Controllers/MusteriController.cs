0cfe47cd58a45d4ed6020ac1383cdfa03e8ca9be
Özgür <39501502+cakicio7gur@users.noreply.github.com>
2019-10-24 11:29:03 +0300

    Done
    Project done.

--- MVCBankProject/MVCBankProject/Controllers/MusteriController.cs
+++ MVCBankProject/MVCBankProject/Controllers/MusteriController.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using ModelLayer.DTOs;
+using ModelLayer.Models;
 
 namespace MVCBankProject.Controllers
 {
     public class MusteriController : Controller
     {
         // GET: Musteri
-        public ActionResult Hesap()
+        public ActionResult Hesap(int musteriNo)
         {
-            return View();
+            IEnumerable<ModelLayer.DTOs.HesapDTO> hesaplar = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63237/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Hesap/GetByMusteriNo?musteriNo=100001");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<ModelLayer.DTOs.HesapDTO>>();
+                    readTask.Wait();
+
+                    hesaplar = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    hesaplar = Enumerable.Empty<ModelLayer.DTOs.HesapDTO>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(hesaplar);
         }
         public ActionResult HesapDetay()
         {
@@ -25,8 +54,22 @@ namespace MVCBankProject.Controllers
         {
             return View();
         }
-        public ActionResult Menu()
+        public ActionResult Menu(int id)
         {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63237/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Hesap/GetToplamBakiye/"+id.ToString());
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<String>();
+                    readTask.Wait();
+                    ViewBag.toplamBakiye = readTask.Result;
+                }
+            }
             return View();
         }
 
--- MVCBankProject/MVCBankProject/Controllers/SecurityController.cs
+++ MVCBankProject/MVCBankProject/Controllers/SecurityController.cs
@@ -1,17 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace MVCBankProject.Controllers
 {
     public class SecurityController : Controller
     {
         // GET: Security
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string tckn,string sifre)
+        {
+            /*HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:63237/api/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-ww-from-urlencoded"));
+
+            var postData = new Dictionary<string, string>();
+            postData.Add("grant_type", "sifre");
+            postData.Add("username", tckn);
+            postData.Add("sifre", sifre);
+            HttpContent content = new FormUrlEncodedContent(postData);
+            HttpResponseMessage response = client.PostAsync("token", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Token token = response.Content.ReadAsAsync<Token>().Result;
+                Session["token"] = token.access_token;
+            }
+            else
+            {
+                return View();
+            }*/
+            return RedirectToAction("Menu", "Musteri");
         }
     }
 }
